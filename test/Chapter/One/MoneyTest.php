06ad9d0ffe4dafba8e27ab0264f718a0caaf2bb8
Kentaro Minami <mkentaro555@gmail.com>
2020-07-03 16:42:33 +0900

    MoneyTestクラスの実装。

--- test/Chapter/One/MoneyTest.php
+++ test/Chapter/One/MoneyTest.php
@@ -1,1 +1,20 @@
 <?php
+namespace Test\Chapter\One;
+
+use App\Chapter\One\Dollar;
+use PHPUnit\Framework\TestCase;
+
+/**
+ *
+ */
+class MoneyTest extends TestCase
+{
+
+     public function testMultiplication()
+    {
+        $five = new Dollar(5);
+        $five->times(2);
+        //assertEqualsは==のため、assertSame(===)の方が方まで見れて良い。
+        $this->assertSame(10, $five->amount);
+    }
+}

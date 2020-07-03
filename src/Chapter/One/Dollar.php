0453e0d297d97f3896b84b8da0fb4bc04d69d7a3
Kentaro Minami <mkentaro555@gmail.com>
2020-07-03 16:42:16 +0900

    Dollarクラスの実装。

--- src/Chapter/One/Dollar.php
+++ src/Chapter/One/Dollar.php
@@ -1,2 +1,25 @@
 <?php
+namespace App\Chapter\One;
 
+
+// TODO
+// $5 + 10 CHF = $10(レート2:1の場合)
+// $5 * 2 = $10 (済)
+// amountをprivateにする
+// Dollarの副作用どうする？
+// Moneyの丸め処理どうする？
+
+class Dollar
+{
+
+    public $amount;
+    public function __construct(int $amount)
+    {
+        $this->amount = $amount;
+    }
+
+    public function times(int $multipler):void
+    {
+        $this->amount = $this->amount * $multipler;
+    }
+}

09105018839b99c0e65483fbc96b34539ff70868
Kentaro Minami <mkentaro555@gmail.com>
2020-07-03 17:05:29 +0900

    MoneyTestの処理作成。

---
+++ test/Dollar/MoneyTest.php
@@ -0,0 +1,22 @@
+<?php
+namespace Test\Dollar;
+
+use App\Dollar\Dollar;
+use PHPUnit\Framework\TestCase;
+
+/**
+ *
+ */
+class MoneyTest extends TestCase
+{
+
+    public function testMultiplication()
+    {
+        $five = new Dollar(5);
+        $product = $five->times(2);
+        //assertEqualsは==のため、assertSame(===)の方が方まで見れて良い。
+        $this->assertSame(10, $product->amount);
+        $product = $five->times(3);
+        $this->assertSame(15, $product->amount);
+    }
+}

d3d75cb9e55702ca0571270f9a0a9b73b95211e8
Kentaro5 <Kentaro5@users.noreply.github.com>
2020-07-10 16:01:06 +0900

    Revert "第六章の内容実装。"

--- src/Dollar/Dollar.php
+++ src/Dollar/Dollar.php
@@ -1,13 +1,26 @@
 <?php
 namespace App\Dollar;
 
-use App\Common\Money\Money;
 
+// TODO
+// $5 + 10 CHF = $10(レート2:1の場合)
+// $5 * 2 = $10 (済)
+// amountをprivateにする(済)
+// Dollarの副作用どうする？ (済)
+// Moneyの丸め処理どうする？
+// equals() (済)
+// hashCode()
+// nullとの等価性比較
+// 他のオブジェクトとの等価性比較
+// 5CHF * 2 = 10CHF (済)
+// DollarとFrancの重複
+// equalsの一般化
+// timesの一般化
 
-
-class Dollar extends Money
+class Dollar
 {
 
+    private $amount;
     public function __construct(int $amount)
     {
         $this->amount = $amount;
@@ -19,5 +32,9 @@ class Dollar extends Money
 
     }
 
-
+    public function equals(Object $object):Bool
+    {
+        $dollar = $object;
+        return $this->amount === $dollar->amount;
+    }
 }

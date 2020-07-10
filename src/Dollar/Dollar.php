7455af440c8ef039166c13fd4caf10ba06fe7374
Kentaro Minami <mkentaro555@gmail.com>
2020-07-10 15:53:53 +0900

    Read.mdに内容を転記。

--- src/Dollar/Dollar.php
+++ src/Dollar/Dollar.php
@@ -3,20 +3,6 @@ namespace App\Dollar;
 
 use App\Common\Money\Money;
 
-// TODO
-// $5 + 10 CHF = $10(レート2:1の場合)
-// $5 * 2 = $10 (済)
-// amountをprivateにする(済)
-// Dollarの副作用どうする？ (済)
-// Moneyの丸め処理どうする？
-// equals() (済)
-// hashCode()
-// nullとの等価性比較
-// 他のオブジェクトとの等価性比較
-// 5CHF * 2 = 10CHF (済)
-// DollarとFrancの重複
-// equalsの一般化
-// timesの一般化
 
 
 class Dollar extends Money
